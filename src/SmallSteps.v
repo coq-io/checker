e09402352705d37475ff0ebc0a7c6373d210c3b1
Guillaume Claret <dev@clarus.me>
2015-04-15 11:05:46 +0200

    Small-steps semantics with events

--- src/SmallSteps.v
+++ src/SmallSteps.v
@@ -27,6 +27,27 @@ Module C.
   Arguments Choose {E A} _ _.
 
   Module Step.
+    Inductive t {E : Effect.t} {A : Type} (c : Effect.command E)
+      (a : Effect.answer E c) : C.t E A -> C.t E A -> Type :=
+    | Call : forall h,
+      t c a (C.Call c h) (h a)
+    | JoinLeft : forall B C (x : C.t E B) (y : C.t E C) h x',
+      t c a (A := B) x x' ->
+      t c a (C.Join x y h) (C.Join x' y h)
+    | JoinRight : forall B C (x : C.t E B) (y : C.t E C) h y',
+      t c a (A := C) y y' ->
+      t c a (C.Join x y h) (C.Join x y' h)
+    | Join : forall B C (x : B) (y : C) h,
+      t c a (C.Join (C.Ret x) (C.Ret y) h) (h (x, y))
+    | ChooseLeft : forall x1 x2 x1',
+      t c a x1 x1' ->
+      t c a (C.Choose x1 x2) x1'
+    | ChooseRight : forall x1 x2 x2',
+      t c a x2 x2' ->
+      t c a (C.Choose x1 x2) x2'.
+  End Step.
+
+  Module ModelStep.
     Inductive t {E : Effect.t} {S : Type} (m : Model.t E S) {A : Type} (s : S)
       : C.t E A -> S -> C.t E A -> Type :=
     | Call : forall c h,
@@ -46,5 +67,5 @@ Module C.
     | ChooseRight : forall x1 x2 s' x2',
       t m s x2 s' x2' ->
       t m s (C.Choose x1 x2) s' x2'.
-  End Step.
+  End ModelStep.
 End C.
