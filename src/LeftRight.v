cc9f9aef5a01eecc5757cc4f2efdd38f93efc165
Guillaume Claret <dev@clarus.me>
2015-04-18 16:43:05 +0200

    Definition of a compilation predicate

--- src/LeftRight.v
+++ src/LeftRight.v
@@ -62,11 +62,39 @@ Module Step.
     t (LeftRight.JoinRight x y) (LeftRight.join x y').
 End Step.
 
-Fixpoint compile {E A} (x : C.t E A) : t E A :=
-  match x with
-  | C.Ret _ v => Ret v
-  | C.Call c => Call c
-  | C.Let _ _ x f => Let (compile x) (fun x => compile (f x))
-  | C.Choose _ x1 x2 => Choose (compile x1) (compile x2)
-  | C.Join _ _ x y => join (compile x) (compile y)
-  end.
+Module Compile.
+  Inductive t {E : Effect.t} : forall {A}, C.t E A -> LeftRight.t E A -> Type :=
+  | Ret : forall A (v : A), t (C.Ret _ v) (LeftRight.Ret v)
+  | Call : forall c, t (C.Call c) (LeftRight.Call c)
+  | Let : forall A B (x : C.t E A) (f : A -> C.t E B) c_x c_f,
+    t x c_x -> (forall v_x, t (f v_x) (c_f v_x)) ->
+    t (C.Let _ _ x f) (LeftRight.Let c_x c_f)
+  | Choose : forall A (x1 x2 : C.t E A) c_x1 c_x2,
+    t x1 c_x1 -> t x2 c_x2 ->
+    t (C.Choose _ x1 x2) (LeftRight.Choose c_x1 c_x2)
+  | Join : forall A B (x : C.t E A) (y : C.t E B) c_x c_y,
+    t x c_x -> t y c_y ->
+    t (C.Join _ _ x y) (LeftRight.join c_x c_y).
+
+  Fixpoint compile {E A} (x : C.t E A) : LeftRight.t E A :=
+    match x with
+    | C.Ret _ v => LeftRight.Ret v
+    | C.Call c => LeftRight.Call c
+    | C.Let _ _ x f => LeftRight.Let (compile x) (fun x => compile (f x))
+    | C.Choose _ x1 x2 => LeftRight.Choose (compile x1) (compile x2)
+    | C.Join _ _ x y => LeftRight.join (compile x) (compile y)
+    end.
+
+  Fixpoint make {E A} (x : C.t E A) : t x (compile x).
+    destruct x.
+    - apply Ret.
+    - apply Call.
+    - now apply Let.
+    - now apply Choose.
+    - now apply Join.
+  Defined.
+End Compile.
+
+Module Sound.
+
+End Sound.
