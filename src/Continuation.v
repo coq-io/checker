f6bb9a3ee6cb028022c93443018a7f5db1c67b43
Guillaume Claret <dev@clarus.me>
2015-04-20 19:13:40 +0200

    New version of step

--- src/Continuation.v
+++ src/Continuation.v
@@ -59,75 +59,109 @@ Module Location.
   | Join : t -> t.
 
   Module Valid.
-    Inductive t {E A} (c : Effect.command E) : Location.t -> C.t E A -> Prop :=
-    | Call : forall h, t c Location.Call (C.Call c h)
+    Inductive t {E A} : Location.t -> C.t E A -> Prop :=
+    | Call : forall c h, t Location.Call (C.Call c h)
     | ChooseLeft : forall l (x1 x2 : C.t E A),
-      t c l x1 ->
-      t c (Location.ChooseLeft l) (C.Choose x1 x2)
+      t l x1 ->
+      t (Location.ChooseLeft l) (C.Choose x1 x2)
     | ChooseRight : forall l (x1 x2 : C.t E A),
-      t c l x2 ->
-      t c (Location.ChooseRight l) (C.Choose x1 x2)
+      t l x2 ->
+      t (Location.ChooseRight l) (C.Choose x1 x2)
     | JoinLeft : forall l B C (x : C.t E B) (y : C.t E C) k,
-      t (A := B) c l x ->
-      t c (Location.JoinLeft l) (C.Join x y k)
+      t (A := B) l x ->
+      t (Location.JoinLeft l) (C.Join x y k)
     | JoinRight : forall l B C (x : C.t E B) (y : C.t E C) k,
-      t (A := C) c l y ->
-      t c (Location.JoinRight l) (C.Join x y k)
+      t (A := C) l y ->
+      t (Location.JoinRight l) (C.Join x y k)
     | Join : forall l B C (v_x : B) (v_y : C) k,
-      t c l (k (v_x, v_y)) ->
-      t c (Location.Join l) (C.Join (C.Ret v_x) (C.Ret v_y) k).
-
-    Definition inversion_call {E A} {c l c' h}
-      (H : t c l (C.Call (E := E) (A := A) c' h)) : c = c'.
-      now inversion_clear H.
-    Qed.
+      t l (k (v_x, v_y)) ->
+      t (Location.Join l) (C.Join (C.Ret v_x) (C.Ret v_y) k).
   End Valid.
 
-  Fixpoint step {E A} c (l : t) (x : C.t E A) (a : Effect.answer E c)
-    (H : Valid.t c l x) {struct l} : C.t E A.
-    destruct l; destruct x as [v | c' h | x1 x2 | B C x y k];
+  Fixpoint step {E A} (l : t) (x : C.t E A) (H : Valid.t l x)
+    : {c : Effect.command E & Effect.answer E c -> C.t E A}.
+    destruct l; destruct x as [v | c h | x1 x2 | B C x y k];
       try (assert False by inversion H; tauto).
-    - rewrite (Valid.inversion_call H) in a.
-      exact (h a).
-    - refine (C.Choose _ x2).
-      apply (step _ _ c l x1 a).
-      now inversion H.
-    - refine (C.Choose x1 _).
-      apply (step _ _ c l x2 a).
-      now inversion H.
-    - refine (C.Join _ y k).
-      apply (step _ _ c l x a).
+    - exists c.
+      exact h.
+    - apply (step _ _ l x1).
       exact (
-        match H in Valid.t _ l x return
+        match H in Valid.t l x return
           match l with
-          | Location.JoinLeft l =>
+          | ChooseLeft l =>
             match x with
-            | C.Join _ _ x _ _  => Valid.t c l x
+            | C.Choose x1 _ => Valid.t l x1
             | _ => False
             end
           | _ => True
           end : Prop with
-        | Valid.JoinLeft _ _ _ _ _ _ H => H
+        | Valid.ChooseLeft _ _ _ H => H
         | _ => I
         end).
-    - refine (C.Join x _ k).
-      apply (step _ _ c l y a).
+    - apply (step _ _ l x2).
       exact (
-        match H in Valid.t _ l x return
+        match H in Valid.t l x return
           match l with
-          | Location.JoinRight l =>
+          | ChooseRight l =>
             match x with
-            | C.Join _ _ _ y _  => Valid.t c l y
+            | C.Choose _ x2 => Valid.t l x2
             | _ => False
             end
           | _ => True
           end : Prop with
-        | Valid.JoinRight _ _ _ _ _ _ H => H
+        | Valid.ChooseRight _ _ _ H => H
         | _ => I
         end).
+    - refine (let (c, x') := step _ _ l x _ in _).
+      + exact (
+          match H in Valid.t l x return
+            match l with
+            | JoinLeft l =>
+              match x with
+              | C.Join _ _ x _ _ => Valid.t l x
+              | _ => False
+              end
+            | _ => True
+            end : Prop with
+          | Valid.JoinLeft _ _ _ _ _ _ H => H
+          | _ => I
+          end).
+      + exists c.
+        intro a.
+        exact (C.Join (x' a) y k).
+    - refine (let (c, y') := step _ _ l y _ in _).
+      + exact (
+          match H in Valid.t l x return
+            match l with
+            | JoinRight l =>
+              match x with
+              | C.Join _ _ _ y _ => Valid.t l y
+              | _ => False
+              end
+            | _ => True
+            end : Prop with
+          | Valid.JoinRight _ _ _ _ _ _ H => H
+          | _ => I
+          end).
+      + exists c.
+        intro a.
+        exact (C.Join x (y' a) k).
     - destruct x as [v_x | | |]; try (assert False by inversion H; tauto).
       destruct y as [v_y | | |]; try (assert False by inversion H; tauto).
-      exact (k (v_x, v_y)).
+      apply (step _ _ l (k (v_x, v_y))).
+      exact (
+        match H in Valid.t l x return
+          match l with
+          | Join l =>
+            match x with
+            | C.Join _ _ (C.Ret v_x) (C.Ret v_y) k => Valid.t l (k (v_x, v_y))
+            | _ => False
+            end
+          | _ => True
+          end : Prop with
+        | Valid.Join _ _ _ _ _ _ H => H
+        | _ => I
+        end).
   Defined.
 
   Fixpoint option_step {E A} (l : Location.t) (x : C.t E A)
@@ -172,4 +206,6 @@ Module Location.
       | _ => None
       end
     end.
+
+  (*Fixpoint option_step_ok {E A} (l : Location.t) (x : C.t E A) c (H : )*)
 End Location.
