8c2d475abe4ee592161c8f71ad82f1bbbdcfddaa
Guillaume Claret <dev@clarus.me>
2015-04-20 17:46:08 +0200

    Step without result value

--- src/Continuation.v
+++ src/Continuation.v
@@ -43,21 +43,26 @@ End LastStep.
 
 Module Step.
   Inductive t {E A} (c : Effect.command E) (a : Effect.answer E c)
-    : C.t E A -> C.t E A -> Prop :=
-  | Call : forall h, t c a (C.Call c h) (h a)
-  | ChooseLeft : forall (x1 x2 x1' : C.t E A),
-    t c a x1 x1' ->
-    t c a (C.Choose x1 x2) x1'
-  | ChooseRight : forall (x1 x2 x2' : C.t E A),
-    t c a x2 x2' ->
-    t c a (C.Choose x1 x2) x2'
-  | JoinLeft : forall B C (x x' : C.t E B) (y : C.t E C) k,
-    t (A := B) c a x x' ->
-    t c a (C.Join x y k) (C.Join x' y k)
-  | JoinRight : forall B C (x : C.t E B) (y y' : C.t E C) k,
-    t (A := C) c a y y' ->
-    t c a (C.Join x y k) (C.Join x y' k)
-  | Join : forall B C (x : C.t E B) v_x (y : C.t E C) v_y k z,
-    LastStep.t x v_x -> LastStep.t y v_y -> t c a (k (v_x, v_y)) z ->
-    t c a (C.Join x y k) z.
+    : C.t E A -> Prop :=
+  | Call : forall h, t c a (C.Call c h)
+  | ChooseLeft : forall (x1 x2 : C.t E A),
+    t c a x1 ->
+    t c a (C.Choose x1 x2)
+  | ChooseRight : forall (x1 x2 : C.t E A),
+    t c a x2 ->
+    t c a (C.Choose x1 x2)
+  | JoinLeft : forall B C (x : C.t E B) (y : C.t E C) k,
+    t (A := B) c a x ->
+    t c a (C.Join x y k)
+  | JoinRight : forall B C (x : C.t E B) (y : C.t E C) k,
+    t (A := C) c a y ->
+    t c a (C.Join x y k)
+  | Join : forall B C (x : C.t E B) v_x (y : C.t E C) v_y k,
+    LastStep.t x v_x -> LastStep.t y v_y -> t c a (k (v_x, v_y)) ->
+    t c a (C.Join x y k).
+
+  Definition inversion_call_c {E A} c a c' h
+    (H : t c a (C.Call (E := E) (A := A) c' h)) : c = c'.
+    now inversion_clear H.
+  Qed.
 End Step.
