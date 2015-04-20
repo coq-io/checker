287d20fc9ea50db34f4de5b8b6a0765a8aee9d99
Guillaume Claret <dev@clarus.me>
2015-04-20 18:22:28 +0200

    step function almost defined

--- src/Continuation.v
+++ src/Continuation.v
@@ -39,30 +39,104 @@ Module LastStep.
     | Ret v'' => eq_refl
     | _ => I
     end.
+
+  (*Fixpoint eval {E A} (x : C.t E A) : option A :=
+    match x with
+    | C.Ret v => Some v
+    | C.Call 
+    end.*)
 End LastStep.
 
-Module Step.
-  Inductive t {E A} (c : Effect.command E) (a : Effect.answer E c)
-    : C.t E A -> Prop :=
-  | Call : forall h, t c a (C.Call c h)
-  | ChooseLeft : forall (x1 x2 : C.t E A),
-    t c a x1 ->
-    t c a (C.Choose x1 x2)
-  | ChooseRight : forall (x1 x2 : C.t E A),
-    t c a x2 ->
-    t c a (C.Choose x1 x2)
-  | JoinLeft : forall B C (x : C.t E B) (y : C.t E C) k,
-    t (A := B) c a x ->
-    t c a (C.Join x y k)
-  | JoinRight : forall B C (x : C.t E B) (y : C.t E C) k,
-    t (A := C) c a y ->
-    t c a (C.Join x y k)
-  | Join : forall B C (x : C.t E B) v_x (y : C.t E C) v_y k,
-    LastStep.t x v_x -> LastStep.t y v_y -> t c a (k (v_x, v_y)) ->
-    t c a (C.Join x y k).
+Module Location.
+  Inductive t : Set :=
+  | Call : t
+  | ChooseLeft : t -> t
+  | ChooseRight : t -> t
+  | JoinLeft : t -> t
+  | JoinRight : t -> t
+  | Join : t -> t.
 
-  Definition inversion_call_c {E A} c a c' h
-    (H : t c a (C.Call (E := E) (A := A) c' h)) : c = c'.
-    now inversion_clear H.
-  Qed.
-End Step.
+  Module Valid.
+    Inductive t {E A} (c : Effect.command E) : Location.t -> C.t E A -> Prop :=
+    | Call : forall h, t c Location.Call (C.Call c h)
+    | ChooseLeft : forall l (x1 x2 : C.t E A),
+      t c l x1 ->
+      t c (Location.ChooseLeft l) (C.Choose x1 x2)
+    | ChooseRight : forall l (x1 x2 : C.t E A),
+      t c l x2 ->
+      t c (Location.ChooseRight l) (C.Choose x1 x2)
+    | JoinLeft : forall l B C (x : C.t E B) (y : C.t E C) k,
+      t (A := B) c l x ->
+      t c (Location.JoinLeft l) (C.Join x y k)
+    | JoinRight : forall l B C (x : C.t E B) (y : C.t E C) k,
+      t (A := C) c l y ->
+      t c (Location.JoinRight l) (C.Join x y k)
+    | Join : forall l B C (x : C.t E B) v_x (y : C.t E C) v_y k,
+      LastStep.t x v_x -> LastStep.t y v_y ->
+      t c l (k (v_x, v_y)) ->
+      t c (Location.Join l) (C.Join x y k).
+
+    Definition inversion_call {E A} {c l c' h}
+      (H : t c l (C.Call (E := E) (A := A) c' h)) : c = c'.
+      now inversion_clear H.
+    Qed.
+  End Valid.
+
+  Fixpoint step {E A} c (l : t) (x : C.t E A) (a : Effect.answer E c) (H : Valid.t c l x) {struct l} : C.t E A.
+    destruct l; destruct x as [v | c' h | x1 x2 | B C x y k];
+      try (assert False by inversion H; tauto).
+    - rewrite (Valid.inversion_call H) in a.
+      exact (h a).
+    - refine (C.Choose _ x2).
+      apply (step _ _ c l x1 a).
+      now inversion H.
+    - refine (C.Choose x1 _).
+      apply (step _ _ c l x2 a).
+      now inversion H.
+    - refine (C.Join _ y k).
+      apply (step _ _ c l x a).
+      exact (
+        match H in Valid.t _ l x return
+          match l with
+          | Location.JoinLeft l =>
+            match x with
+            | C.Join _ _ x _ _  => Valid.t c l x
+            | _ => False
+            end
+          | _ => True
+          end : Prop with
+        | Valid.JoinLeft _ _ _ _ _ _ H => H
+        | _ => I
+        end).
+    - refine (C.Join x _ k).
+      apply (step _ _ c l y a).
+      exact (
+        match H in Valid.t _ l x return
+          match l with
+          | Location.JoinRight l =>
+            match x with
+            | C.Join _ _ _ y _  => Valid.t c l y
+            | _ => False
+            end
+          | _ => True
+          end : Prop with
+        | Valid.JoinRight _ _ _ _ _ _ H => H
+        | _ => I
+        end).
+    - exact (
+      refine (
+        match H in Valid.t _ l x return
+          match l with
+          | Location.JoinRight l =>
+            match x with
+            | C.Join _ _ _ y _  => Valid.t c l y
+            | _ => False
+            end
+          | _ => True
+          end : Prop with
+        | Valid.JoinRight _ _ _ _ _ _ H => H
+        | _ => I
+        end).
+      
+  Defined.
+End Location.
