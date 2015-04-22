0c710a172f4ace2c0867f6dee292f3fa26427592
Guillaume Claret <dev@clarus.me>
2015-04-22 23:17:36 +0200

    Draft; we need to assume that a starting position is valid

--- src/Bisimulation.v
+++ src/Bisimulation.v
@@ -7,27 +7,32 @@ Module ToChoose.
   Module Last.
     Fixpoint map {E A B} {p x} {f : A -> B} (s : Choose.Last.Start.t p x)
       : Choose.Last.Start.t (E := E) p (Choose.map x f).
-    Admitted.
+      destruct s; simpl.
+      - apply Choose.Last.Start.Ret.
+      - apply Choose.Last.Start.ChooseLeft.
+        now apply map.
+      - apply Choose.Last.Start.ChooseRight.
+        now apply map.
+    Defined.
 
-    Fixpoint bind {E A B} {p_x x} {v_x} {p_f f}
+    Fixpoint bind {E A B} {p_x x} {p_f f}
       (s_x : Choose.Last.Start.t (A := A) p_x x)
-      (s_f : Choose.Last.Start.t (A := B) p_f (f v_x))
+      (s_f : Choose.Last.Start.t (A := B) p_f (f (Choose.Last.eval s_x)))
       : Choose.Last.Start.t (E := E) (Choose.Last.Path.bind p_x p_f)
         (Choose.bind x f).
-    Admitted.
+      destruct s_x; simpl in *.
+      - trivial.
+      - apply Choose.Last.Start.ChooseLeft.
+        eapply bind.
+        apply s_f.
+      - apply Choose.Last.Start.ChooseRight.
+        eapply bind.
+        apply s_f.
+    Defined.
 
     Fixpoint join {E A B} {p_x} {x : Choose.t E A} {p_y} {y : Choose.t E B}
       (s_x : Choose.Last.Start.t p_x x) (s_y : Choose.Last.Start.t p_y y)
       : Choose.Last.Start.t (Choose.Last.Path.bind p_x p_y) (Choose.join x y).
-    Admitted.
-
-    Fixpoint bind' {E A B} {p_x x v_x} {p_f f}
-      (s_x : C.Last.Start.t p_x x) (s_f : C.Last.Start.t p_f (f v_x))
-      : Choose.Last.Start.t
-        (Choose.Last.Path.bind
-          (Compile.Path.Last.to_choose p_x) (Compile.Path.Last.to_choose p_f))
-        (Choose.bind (E := E) (A := A) (B := B)
-          (Compile.to_choose x) (fun v => Compile.to_choose (f v))).
     Admitted.
 
     Fixpoint to_choose {E A} {p : C.Last.Path.t} {x : C.t E A}
@@ -36,7 +41,9 @@ Module ToChoose.
         (Compile.Path.Last.to_choose p) (Compile.to_choose x).
       destruct s; simpl.
       - apply Choose.Last.Start.Ret.
-      - apply (bind (v_x := v_x)).
+      - eapply bind.
+        apply s2.
+        
         + now apply to_choose.
         + now apply to_choose.
       - apply Choose.Last.Start.ChooseLeft.
@@ -48,4 +55,6 @@ Module ToChoose.
         + now apply to_choose.
     Qed.
   End Last.
+
+  Fixpoint to_choose {E A} {
 End ToChoose.
