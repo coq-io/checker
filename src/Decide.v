f553ebd45dc1b6e423f47c1a5352b54c0d8cd859
Guillaume Claret <dev@clarus.me>
2015-04-26 20:31:27 +0200

    A proof of dead_lock_free_ok in a simpler case

--- src/Decide.v
+++ src/Decide.v
@@ -3,6 +3,7 @@ Require Import Io.All.
 Require Choose.
 Require DeadLockFree.
 Require Model.
+Require NoDeps.
 Require Import Semantics.
 
 Fixpoint not_stuck {E S A} {m : Model.t E S} (dec : Model.Dec.t m) (s : S)
@@ -57,48 +58,31 @@ Defined.
   (H_x : Choose.Step.t m s x x' s') : dead_lock_free dec s' x' = true.
 Admitted.*)
 
-(*Fixpoint dead_lock_free_ok {E S A} {m : Model.t E S} {dec : Model.Dec.t m}
-  {s : S} {x : Choose.t E A} (H : dead_lock_free dec s x = true)
-  : DeadLockFree.Choose2.t m s x.
+Fixpoint dead_lock_free_ok {X Y S A} {m : Model.t (NoDeps.E X Y) S}
+  {dec : Model.Dec.t m} {s : S} {x : Choose.t (NoDeps.E X Y) A}
+  (H : dead_lock_free dec s x = true) : DeadLockFree.Choose2.t m s x.
   destruct (proj1 (andb_true_iff _ _) H) as [H_not_stuck H_aux].
   destruct (not_stuck_ok H_not_stuck) as [[v H_v] | [c [x' [s' H_x]]]].
   - now apply (DeadLockFree.Choose2.Ret _ _ _ v).
   - apply (DeadLockFree.Choose2.Call _ _ _ _ x' s' H_x).
     clear c x' s' H_x H H_not_stuck.
-    intros c x' s' H_x.
-    destruct x as [v | c' h | x1 x2]; simpl in H_aux.
+    induction x; intros c' x' s' H_x; simpl in H_aux.
     + inversion_clear H_x.
       inversion H0.
     + inversion_clear H_x.
-      inversion_clear H0.
-      assert (H_eq : c' = c) by (destruct falso).
-      rewrite H_eq in h.
-      inversion H1.
-      
-      generalize H_aux.
-      clear H H_not_stuck H_aux.
-      refine (
-        match H_x in Choose.Step.t _ _ x x' s' return
-          match x with
-          | Choose.Call c h =>
-            match dec c s with
-| left H0 =>
-    not_stuck dec (Model.state m c s H0) (h (Model.answer m c s H0)) &&
-    aux dec (Model.state m c s H0) (h (Model.answer m c s H0))
-| right _ => true
-end = true -> DeadLockFree.Choose2.t m s' x'
-          | _ => True
-          end : Prop with
-        | Choose.Step.New (Choose.Call _ _) _ _ _ _ _ => _
-        | _ => I
-        end).
-      destruct H_x.
+      replace c with c' in * by (destruct falso).
       inversion_clear H1.
-      apply (dead_lock_free_ok _ _ _ _ dec).
-      assert
-      replace c0 with c by (destruct falso).
-      simpl.
-    
-    apply (dead_lock_free_ok _ _ _ _ dec).
-    apply (aux_ok H_aux H_x).
-Qed.*)
+      destruct (dec c' s) as [H_pre | H_not_pre]; simpl in H_aux.
+      * replace H_pre with H0 in H_aux by (destruct falso).
+        now apply (dead_lock_free_ok _ _ _ _ _ dec).
+      * destruct (H_not_pre H0).
+    + inversion_clear H_x.
+      destruct (proj1 (andb_true_iff _ _) H_aux) as [H_x1 H_x2].
+      inversion_clear H0.
+      * apply (IHx1 H_x1 c').
+        eapply Choose.Step.New.
+        apply H1.
+      * apply (IHx2 H_x2 c').
+        eapply Choose.Step.New.
+        apply H1.
+Qed.
